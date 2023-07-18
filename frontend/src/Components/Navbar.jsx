fdadd2972871312916c2d33d1e660f39197ab4d1
Swapnil Wakchaure <swapnilwakchaure2411999@gmail.com>
2023-07-18 19:39:33 +0530

    register and login frontend part done

---
+++ frontend/src/Components/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return (
+        <div>
+            Navbar Page
+        </div>
+    )
+}
+
+export default Navbar;
---
+++ frontend/src/Pages/Register.jsx
@@ -0,0 +1,197 @@
+import { useState } from "react";
+import { FiUser } from "react-icons/fi";
+import { MdOutlineMail } from "react-icons/md";
+import { BsEye, BsEyeSlash } from "react-icons/bs";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+
+const Register = () => {
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [passType, setPassType] = useState('password');
+    const [showPass, setShowPass] = useState(false);
+
+    const navigate = useNavigate();
+
+    const handleShowPassword = () => {
+        setPassType('text');
+        setShowPass(!showPass);
+    };
+
+    const handleHidePassword = () => {
+        setPassType('password');
+        setShowPass(!showPass)
+    };
+
+    const handleRegister = () => {
+        navigate('/register');
+    }
+
+    const handleLogin = () => {
+        navigate('/login');
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (name && email && password) {
+            const payload = { name, email, password };
+            console.log('payload: ', payload);
+
+            setName('');
+            setEmail('');
+            setPassword('');
+        } else {
+            alert('All fields are required');
+        }
+    }
+
+    return (
+        <Main>
+            <Form onSubmit={handleSubmit}>
+
+                <AuthRoute>
+                    <Button
+                        color="white"
+                        left="20px"
+                        onClick={handleRegister}
+                    >
+                        Register
+                    </Button>
+                    <Button
+                        right="20px"
+                        onClick={handleLogin}
+                    >
+                        Log In
+                    </Button>
+                </AuthRoute>
+
+                <Box>
+                    <Input
+                        autoFocus
+                        type="text"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        placeholder="Enter Name"
+                    />
+                    <FiUser style={{
+                        position: 'absolute',
+                        fontSize: '18px'
+                    }} />
+                </Box>
+                <Box>
+                    <Input
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder="Enter Email"
+                    />
+                    <MdOutlineMail style={{
+                        position: 'absolute',
+                        fontSize: '18px'
+                    }} />
+                </Box>
+                <Box>
+                    <Input
+                        type={passType}
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        placeholder="Enter Password"
+                    />
+                    {!showPass ?
+                        <BsEye
+                            onClick={handleShowPassword}
+                            style={{
+                                position: 'absolute',
+                                fontSize: '18px'
+                            }}
+                        /> :
+                        <BsEyeSlash
+                            onClick={handleHidePassword}
+                            style={{
+                                position: 'absolute',
+                                fontSize: '18px'
+                            }}
+                        />
+                    }
+                </Box>
+
+                <Submit type="submit" value="Register" />
+            </Form>
+        </Main>
+    )
+}
+
+export default Register;
+
+
+const Main = styled.div`
+  width: 100%;
+  height: 85vh;
+  background: #FAFAFA;
+  padding-top: 70px;
+`
+
+const Form = styled.form`
+  width: 90%;
+  max-width: 390px;
+  margin: auto;
+  border-radius: 20px;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  border: none;
+  background: white;
+`
+
+const AuthRoute = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 30px;
+`
+
+const Button = styled.button`
+  width: 100%;
+  height: 50px;
+  font-size: 18px;
+  font-weight: 600;
+  border: none;
+  background: ${(props) => props.color};
+  border-top-left-radius: ${(props) => props.left};
+  border-top-right-radius: ${(props) => props.right};
+`
+
+const Box = styled.div`
+  border: 1px solid;
+  width: 80%;
+  max-width: 376px;
+  margin: 20px auto;
+  padding: 6px 20px;
+  display: flex;
+  align-items: center;
+  border-radius: 5px;
+`
+
+const Input = styled.input`
+  width: 100%;
+  padding: 10px 45px;
+  border: 0;
+  outline: 0;
+  font-size: 17px;
+`
+
+const Submit = styled.input`
+  display: block;
+  width: 100%;
+  padding: 14px 20px;
+  margin: auto;
+  margin-top: 30px;
+  border: none;
+  font-size: 17px;
+  font-weight: 600;
+  letter-spacing: 0.1rem;
+  border-bottom-left-radius: 20px;
+  border-bottom-right-radius: 20px;
+`
+
